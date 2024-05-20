ad73e166cd181eb70ccfb0998ecc3e6c0d840716
Sean Sophearom <sean.sophearom77@gmail.com>
2024-05-20 23:52:33 +0700

    setup shadcn-vue & eslint & install packages for future

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"]
-})
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "shadcn-nuxt",
+    "@pinia/nuxt",
+    "@sidebase/nuxt-auth",
+    "nuxt-mongoose",
+    "@nuxt/eslint"
+  ],
+  auth: { provider: { type: "authjs" } },
+});
