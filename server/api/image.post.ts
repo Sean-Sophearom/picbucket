46d98ef49834dfc5cfc726aa812fe955fd5b8c24
Sean Sophearom <sean.sophearom77@gmail.com>
2024-05-27 00:01:06 +0700

    Save user ID along with uploaded image metadata

--- server/api/image.post.ts
+++ server/api/image.post.ts
@@ -1,7 +1,16 @@
+import { getServerSession } from "#auth";
+
 const isImage = (file: File) => file.type.startsWith("image/");
 
 export default defineWrappedResponseHandler(async (event) => {
   initFirebase();
+
+  const session = await getServerSession(event);
+  let user_id: string | undefined;
+  if (session) {
+    const user = await User.findOne({ email: session.user?.email });
+    user_id = user?._id?.toString();
+  }
 
   const formData = await readFormData(event).catch((_err) => {
     throw new BadRequestError("Please provide a file to upload");
@@ -18,6 +27,7 @@ export default defineWrappedResponseHandler(async (event) => {
     ...snapshot.metadata,
     name: file.name,
     downloadUrl: url,
+    user: user_id,
   });
 
   return {
