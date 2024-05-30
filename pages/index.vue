8e98098915c34759b9a63f7f04c5c0d265d3d82e
Sean Sophearom <sean.sophearom77@gmail.com>
2024-05-30 11:46:01 +0700

    chore: Add homepage background component and update index page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="flex flex-col">
-    <NuxtLink v-if="!isAuthenticated" to="/auth/signup">
-      <button class="text-base">Sign up page</button>
-    </NuxtLink>
-    <NuxtLink v-if="!isAuthenticated" to="/auth/signin">
-      <button class="text-base">Sign in page</button>
-    </NuxtLink>
-    <button v-if="isAuthenticated" @click="handleSignout">Sign out</button>
-  </div>
+  <HomepageBG />
+  <UiContainer class="flex-[99999] flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Effortless Image Uploading<br />Made for Everyone</h1>
+      <p class="mt-6 text-lg leading-8 text-gray-600">Drag and drop your picture here, or Sign Up for extended features.</p>
+      <div class="mt-10 flex items-center justify-center gap-x-6">
+        <NuxtLink :to="isAuthenticated ? '/account' : '/auth/signup'">
+          <UiButton>Get Started</UiButton>
+        </NuxtLink>
+        <button @click="imageUploadStore.open" class="text-sm font-semibold leading-6 text-gray-900">Upload Now →</button>
+      </div>
+    </div>
+  </UiContainer>
 </template>
 
 <script lang="ts" setup>
 const { signOut, status } = useAuth();
 
+const imageUploadStore = useImageUploadStore();
 const isAuthenticated = computed(() => status.value === "authenticated");
 
+definePageMeta({
+  layout: "with-nav",
+});
 const { toast } = useToast();
 
 const handleSignout = async () => {
